11c2023fab1e8f53691fdef20bd520adff575caf
Pratik Bassi <pratik.bassi@mail.utoronto.ca>
2020-03-07 00:43:01 +0000

    added users table

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,7 +1,13 @@
 -- Drop and recreate Users table (Example)
 
 DROP TABLE IF EXISTS users CASCADE;
+
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  phone_number VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  profile_picture VARCHAR(255)  DEFAULT 'https://i.imgur.com/4GyU6gt.mp4'
 );
+
