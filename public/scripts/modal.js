6459ab2b6fd5ef1fe67db60622a7f41f86bb124b
Richard <liao098@gmail.com>
2020-03-09 10:24:31 -0700

    added basic modal functionality with single item

---
+++ public/scripts/modal.js
@@ -0,0 +1,51 @@
+$(() => {
+  const escapeTxt = function(str) {
+    const div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+
+  $('.container-item').click(function(params) {
+    const $this = $(this);
+    const $modal = $('#exampleModalBody');
+    $modal.empty();
+    $.ajax({
+      method: "GET",
+      url: `/items/${$this.data('itemid')}`
+    }).done((items) => {
+      const image_url = escapeTxt(items.image_url);
+      const title = escapeTxt(items.title);
+      const stock = escapeTxt(items.stock);
+      const city = escapeTxt(items.city);
+      const description = escapeTxt(items.description);
+      console.log(image_url, title, stock, city, description);
+      
+      const htmlOutput = `
+      <main class="container single-item">
+        <div class="image">
+          <img src="${image_url}">
+        </div>
+        <div class="title">
+          <h2>${title}</h2>
+          <h4>${stock} In Stock</h4>
+          <h5>Located: ${city}</h5>
+          
+        </div>
+        <div class="message">
+          <button>message</button>
+  
+        </div>
+        <div class="description">
+          <article>
+            <p>${description}</p>
+          </article>
+        </div>
+        <div class="admin-tools">
+          <button>admin</button>
+        </div>
+      </main>
+      `;
+      $modal.append(htmlOutput);
+    });
+  })
+});
