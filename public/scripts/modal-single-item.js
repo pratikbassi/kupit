cb181601c192fd2c01b68ce7e9259491d8026bd0
Richard <liao098@gmail.com>
2020-03-10 15:53:51 -0700

    add icons, refactor modal-single-item.js

--- public/scripts/modal-single-item.js
+++ public/scripts/modal-single-item.js
@@ -5,6 +5,43 @@ $(() => {
     return div.innerHTML;
   };
 
+  const loadSingleItemModal = function(url, params) {};
+
+  const generateSingleItemHTML = obj => {
+    const image_url = escapeTxt(obj.image_url);
+    const title = escapeTxt(obj.title);
+    const stock = escapeTxt(obj.stock);
+    const city = escapeTxt(obj.city);
+    const description = escapeTxt(obj.description);
+    const htmlOutput = `
+    <main class="container single-item">
+      <div class="image">
+        <img src="${image_url}">
+      </div>
+      <div class="title">
+        <h2>${title}</h2>
+        <h4>${stock} In Stock</h4>
+        <h5>Located: ${city}</h5>
+        
+      </div>
+      <div class="message">
+        <button>message</button>
+        <i class="material-icons">favorite</i>
+
+      </div>
+      <div class="description">
+        <article>
+          <p>${description}</p>
+        </article>
+      </div>
+      <div class="admin-tools">
+        <button>admin</button>
+      </div>
+    </main>
+    `;
+    return htmlOutput;
+  };
+
   const $modalBody = $("#modalBody");
   $(".container-items").on("click", ".modal-show", function() {
     event.preventDefault();
@@ -14,39 +51,7 @@ $(() => {
       method: "GET",
       url: `/items/${$this.data("itemid")}`
     }).done(items => {
-      const image_url = escapeTxt(items.image_url);
-      const title = escapeTxt(items.title);
-      const stock = escapeTxt(items.stock);
-      const city = escapeTxt(items.city);
-      const description = escapeTxt(items.description);
-      console.log(image_url, title, stock, city, description);
-
-      const htmlOutput = `
-      <main class="container single-item">
-        <div class="image">
-          <img src="${image_url}">
-        </div>
-        <div class="title">
-          <h2>${title}</h2>
-          <h4>${stock} In Stock</h4>
-          <h5>Located: ${city}</h5>
-          
-        </div>
-        <div class="message">
-          <button>message</button>
-  
-        </div>
-        <div class="description">
-          <article>
-            <p>${description}</p>
-          </article>
-        </div>
-        <div class="admin-tools">
-          <button>admin</button>
-        </div>
-      </main>
-      `;
-      $modalBody.append(htmlOutput);
+      $modalBody.append(generateSingleItemHTML(items));
     });
   });
 });
